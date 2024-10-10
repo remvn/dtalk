b0a63d939766284011f2cf1d40c137b930ef288f
remvn <34063162+remvn@users.noreply.github.com>
2024-10-11 01:41:04 +0700

    wip

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Button>click me</Button>
   </main>
 </template>
