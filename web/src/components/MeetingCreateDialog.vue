359732a49f4a2246431c506a6cab65618d2b23e6
remvn <34063162+remvn@users.noreply.github.com>
2024-10-12 09:09:10 +0700

    wip

---
+++ web/src/components/MeetingCreateDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <Button size="lg" variant="outline">Create meeting</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Create a new Meeting</DialogTitle>
+                <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+            </DialogHeader>
+
+            <DialogFooter>Create</DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import MeetingCreateDialog from '@/components/MeetingCreateDialog.vue'
 import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <main>
-    <Button>click me</Button>
-  </main>
+    <div>TODO: logo, navbar here</div>
+    <div class="flex-grow grid grid-cols-1 md:grid-cols-2">
+        <div class="flex justify-center items-center p-6">
+            <div class="xl:w-4/6">
+                <h1 class="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
+                    Meeting and live-streaming for everyone 🎉
+                </h1>
+                <h2 class="text-xl lg:text-xl pt-4">
+                    Connect, live-stream everywhere instantly without an account
+                </h2>
+                <div class="flex gap-3 pt-4">
+                    <MeetingCreateDialog></MeetingCreateDialog>
+                    <Button size="lg">Broadcast</Button>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center items-center p-6">
+            <div class="xl:w-4/6">
+                <span>TODO: images, info here</span>
+            </div>
+        </div>
+    </div>
 </template>
