36d854768ca3e8fc214cf8bd6928c0a67f1eebc6
remvn <34063162+remvn@users.noreply.github.com>
2024-10-20 02:22:35 +0700

    fix css

--- web/src/components/meeting/MeetingRoomParticipant.vue
+++ web/src/components/meeting/MeetingRoomParticipant.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { cn } from '@/lib/utils'
 import type { MeetingRender } from '@/logic/meeting/meeting-renderer'
 
 const { item } = defineProps<{
@@ -7,7 +8,7 @@ const { item } = defineProps<{
 </script>
 
 <template>
-    <div class="video-container">
+    <div ref="container" class="video-container">
         <video
             v-if="item.videoSrc != null"
             class="video rounded-md"
@@ -16,33 +17,34 @@ const { item } = defineProps<{
         ></video>
         <div
             v-else
-            class="w-[50%] max-w-[12rem] rounded-full bg-secondary aspect-square flex items-center justify-center"
+            :class="
+                cn(
+                    'char-container w-[50%] max-w-[6rem] md:max-w-[12rem]',
+                    'rounded-full bg-secondary aspect-square flex items-center justify-center'
+                )
+            "
         >
-            <span class="char font-light">{{ item.name[0].toUpperCase() }}</span>
+            <span class="text-[40cqw]">{{ item.name[0].toUpperCase() }}</span>
         </div>
         <audio v-if="item.audioSrc != null" :srcObject.prop="item.audioSrc" autoplay></audio>
     </div>
 </template>
 
 <style scoped>
-.char {
-    font-size: 7cqw;
-    line-height: 7cqw;
-}
-
 .video-container {
     width: 100%;
     height: 100%;
-    max-width: 100%;
-    max-height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow-y: hidden;
 }
 
 .video {
     width: auto;
     height: 100%;
+}
+
+.char-container {
+    container-type: inline-size;
 }
 </style>
